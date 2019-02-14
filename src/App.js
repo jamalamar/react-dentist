6d47918afd3e88f35d02dfda1b6403c20c992875
jamalamar <jamal_ab22@hotmail.com>
2019-02-14 13:21:21 -0600

    lab 100% completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 import Home from './Home/home.js'
+import Contact from './Contact/contact.js'
+import Procedures from './Procedures/procedures.js'
+
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <Home></Home>
+          <nav>
+            <Link to="/">Go to Home Page</Link>{' '}
+            <Link to="/procedures">See Our Procedures</Link>{' '}
+            <Link to="/contact">Contact Us!</Link>
+          </nav>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/procedures" component={Procedures} />
+          <Route path="/contact" component={Contact} />
       </div>
+      </Router>
     );
   }
 }
---
+++ src/Procedures/procedures.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Procedures extends Component {
+  render() {
+    return (
+      <div className="Procedures">
+        <h1>Procedures</h1>
+        <p>Procedures page</p>
+      </div>
+    );
+  }
+}
+
+export default Procedures;
